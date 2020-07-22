8e97130aa86454e6a3a7105b7f4355992397c921
abdul1muqeeth <68656591+abdul1muqeeth@users.noreply.github.com>
2020-07-23 00:06:08 +0530

    Add files via upload

---
+++ Untitled (1).py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hi")
+
+
+# In[2]:
+
+
+print("Hello")
+
+
+# In[5]:
+
+
+print('Python\'s World')
+
+
+# In[6]:
+
+
+print("Welcome\nTo World")
+
+
+# In[7]:
+
+
+print("Hello \a\a")
+
+
+# In[8]:
+
+
+name="abdul"
+marks=90
+age=20
+print("the name of person is",name,"marks is",marks,"my age is",age)
+
+
+# In[11]:
+
+
+name="abdul"
+marks=90
+age=20
+print("My name is %s i scure of %d and my age is %d"%(name,marks,age))
+
+
+# In[12]:
+
+
+name="abdul"
+marks=90
+age=20
+print(f"My name is {name} i scure of {marks} and my age is {age}")
+
+
+# In[14]:
+
+
+x=y=10
+
+
+# In[15]:
+
+
+id(x)
+
+
+# In[16]:
+
+
+id(y)
+
+
+# In[17]:
+
+
+
+5**5
+
+
+# In[18]:
+
+
+10+20
+
+
+# In[19]:
+
+
+10-20
+
+
+# In[20]:
+
+
+10/2
+
+
+# In[21]:
+
+
+10/3
+
+
+# In[22]:
+
+
+10%3
+
+
+# In[23]:
+
+
+10//3
+
+
+# In[24]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[25]:
+
+
+c=10
+a=c
+
+
+# In[26]:
+
+
+a==c
+
+
+# In[27]:
+
+
+c<=a
+
+
+# In[28]:
+
+
+a>=b
+
+
+# In[29]:
+
+
+a!=b
+
+
+# In[30]:
+
+
+a=10
+10*40
+
+
+# In[31]:
+
+
+a=240
+b=1
+a|b
+
+
+# In[32]:
+
+
+bin(a)
+
+
+# In[33]:
+
+
+a>20 or 10>9
+
+
+# In[34]:
+
+
+10<9 and 20>m
+
+
+# In[35]:
+
+
+not 10<9
+
+
+# In[39]:
+
+
+str1="abdul"
+"u" in str1
+
+
+# In[40]:
+
+
+str1="abdul"
+"c" not in str1
+
+
+# In[41]:
+
+
+a=10
+b=10
+a is b
+
+
+# In[42]:
+
+
+10+10/2*4
+
+
+# In[43]:
+
+
+10/5
+
+
+# In[44]:
+
+
+2**-1
+
+
+# In[45]:
+
+
+7//3
+
+
+# In[46]:
+
+
+10>8>9
+
+
+# In[ ]:
+
+
+
+
